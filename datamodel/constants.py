93dc96821147246b86b02a495dd8a8520dd706c9
Dani Mateo <64838453+DanMat27@users.noreply.github.com>
2021-01-30 12:46:43 +0100

    RatonGato completo (no se incluyen los __init__)

---
+++ datamodel/constants.py
@@ -0,0 +1,9 @@
+'''
+* Grupo 1363
+* Pareja 8
+* File: constants.py
+'''
+
+ERROR_MESSAGE_ID = 'msg_error'
+JOIN_GAME_ERROR_NOGAME = 'No hay juegos disponibles'
+GAME_SELECTED_SESSION_ID = 'game_selected'
